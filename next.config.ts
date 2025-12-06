0a893ac0c3b743767e54e6f76045ab9772952b60
Sauravsingh1719 <sauravs1719@gmail.com>
2025-12-06 06:54:22 +0530

    config file changed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["assets.aceternity.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+      },
+    ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   staticPageGenerationTimeout: 240,
 };
 
